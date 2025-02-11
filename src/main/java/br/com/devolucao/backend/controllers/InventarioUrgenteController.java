9537e43bfd2117dddcf35478432402f717243ce7
Luan Lucas Barbosa <91332957+LuanBarb0sa@users.noreply.github.com>
2025-02-11 00:14:56 -0300

    feat: Colocando mais um método para evitar a tela em branco

--- src/main/java/br/com/devolucao/backend/controllers/InventarioUrgenteController.java
+++ src/main/java/br/com/devolucao/backend/controllers/InventarioUrgenteController.java
@@ -3,10 +3,13 @@ package br.com.devolucao.backend.controllers;
 import br.com.devolucao.backend.domain.InventarioUrgente;
 import br.com.devolucao.backend.services.EquipamentoService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/inventario-urgente")
@@ -27,6 +30,11 @@ public class InventarioUrgenteController {
         headers.setContentType(MediaType.TEXT_PLAIN);
         headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=inventario_urgente.csv");
         return ResponseEntity.ok().headers(headers).body(csvContent);
+    }
+
+    @GetMapping("/parcial-contagem")
+    public ResponseEntity<?> obterContagem() {
+        return ResponseEntity.ok().body(equipamentoService.obterContagem());
     }
 
 }
--- src/main/java/br/com/devolucao/backend/services/EquipamentoService.java
+++ src/main/java/br/com/devolucao/backend/services/EquipamentoService.java
@@ -58,5 +58,10 @@ public class EquipamentoService {
         return writer.toString();
     }
 
+    public List<InventarioUrgente> obterContagem() {
+        log.info("Obtendo contagem de inventário urgente.");
+        return inventarioUrgenteRepository.findAll();
+    }
+
 }
 
