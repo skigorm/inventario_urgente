27c210487d6edf9b667f7692a8b2041b5c860210
Luan Lucas Barbosa <91332957+LuanBarb0sa@users.noreply.github.com>
2025-02-10 21:16:11 -0300

    feat: Incluindo exportação inventário urgente[

--- src/main/java/br/com/devolucao/backend/controllers/InventarioUrgenteController.java
+++ src/main/java/br/com/devolucao/backend/controllers/InventarioUrgenteController.java
@@ -3,6 +3,8 @@ package br.com.devolucao.backend.controllers;
 import br.com.devolucao.backend.domain.InventarioUrgente;
 import br.com.devolucao.backend.services.EquipamentoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,4 +19,14 @@ public class InventarioUrgenteController {
     public ResponseEntity<?> registrarInventario(@RequestBody InventarioUrgente request) {
         return equipamentoService.registrarInventario(request);
     }
+
+    @GetMapping("/export-csv")
+    public ResponseEntity<String> exportarCsv() {
+        String csvContent = equipamentoService.gerarCsvInventario();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=inventario_urgente.csv");
+        return ResponseEntity.ok().headers(headers).body(csvContent);
+    }
+
 }
--- src/main/java/br/com/devolucao/backend/services/EquipamentoService.java
+++ src/main/java/br/com/devolucao/backend/services/EquipamentoService.java
@@ -5,15 +5,19 @@ import br.com.devolucao.backend.inv_domain.InvUsuario;
 import br.com.devolucao.backend.inv_repositories.InvUsuarioRepository;
 import br.com.devolucao.backend.repositories.InventarioUrgenteRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.StringWriter;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EquipamentoService {
 
     private final InventarioUrgenteRepository inventarioUrgenteRepository;
@@ -37,5 +41,22 @@ public class EquipamentoService {
 
         return ResponseEntity.ok("Inventário registrado com sucesso.");
     }
+
+    public String gerarCsvInventario() {
+
+        log.info("Gerando CSV de inventário urgente.");
+
+        List<InventarioUrgente> invetariosRegistrados = inventarioUrgenteRepository.findAll();
+        StringWriter writer = new StringWriter();
+        writer.append("id,codigo_sap,serial,usuario_id\n");
+        for (InventarioUrgente inventario : invetariosRegistrados) {
+            writer.append(inventario.getId().toString()).append(';')
+                    .append(inventario.getCodigoSap()).append(';')
+                    .append(inventario.getSerial()).append(';')
+                    .append(inventario.getUsuario().getId().toString()).append('\n');
+        }
+        return writer.toString();
+    }
+
 }
 
